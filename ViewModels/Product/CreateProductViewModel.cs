a32710b6b7c7aa41922c54dbbdc75aa4b1eda4e7
Gustavo Felipe Marcial <gustavo.marcial@guapore.business>
2025-08-28 11:42:07 -0400

    DisplayName para o campo Description das ViewModels do Product

--- ViewModels/Product/CreateProductViewModel.cs
+++ ViewModels/Product/CreateProductViewModel.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace CollectApp.ViewModels
 {
     public class CreateProductViewModel
     {
+        [Required]
+        [StringLength(15, MinimumLength = 3, ErrorMessage = "Produto deve ter entre 3 e 15 caracteres")]
+        [DisplayName("Descrição")]
         public string? Description { get; set; }
     }
 }
--- ViewModels/Product/ProductListViewModel.cs
+++ ViewModels/Product/ProductListViewModel.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel;
+
 namespace CollectApp.ViewModels
 {
     public class ProductListViewModel
     {
         public int Id { get; set; }
+
+        [DisplayName("Descrição")]
         public string? Description { get; set; }
     }
 }
