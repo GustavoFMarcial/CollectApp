5e4fff6bac542ec6556fdd0721d1467c72d47a3e
Gustavo Felipe Marcial <gustavo.marcial@guapore.business>
2025-08-28 17:12:35 -0400

    Atualizar mensagem de erro do campo Description do CreteProductViewModel

--- ViewModels/Product/CreateProductViewModel.cs
+++ ViewModels/Product/CreateProductViewModel.cs
@@ -6,7 +6,7 @@ namespace CollectApp.ViewModels
     public class CreateProductViewModel
     {
         [Required]
-        [StringLength(15, MinimumLength = 3, ErrorMessage = "Produto deve ter entre 3 e 15 caracteres")]
+        [StringLength(15, MinimumLength = 3, ErrorMessage = "Descrição deve ter entre 3 e 15 caracteres")]
         [DisplayName("Descrição")]
         public string? Description { get; set; }
     }
