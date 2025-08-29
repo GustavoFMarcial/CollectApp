b53bdb1f1bb0726dd9e7524473d9a8c14fa06b59
Gustavo Felipe Marcial <gustavo.marcial@guapore.business>
2025-08-29 17:00:48 -0400

    Incluir ErrorMessage para campo Description nas ViewModels Create e Edit product

--- ViewModels/Product/CreateProductViewModel.cs
+++ ViewModels/Product/CreateProductViewModel.cs
@@ -5,7 +5,7 @@ namespace CollectApp.ViewModels
 {
     public class CreateProductViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Campo descrição é obrigatório")]
         [StringLength(15, MinimumLength = 3, ErrorMessage = "Descrição deve ter entre 3 e 15 caracteres")]
         [DisplayName("Descrição")]
         public string? Description { get; set; }
--- ViewModels/Product/EditProductViewModel.cs
+++ ViewModels/Product/EditProductViewModel.cs
@@ -7,7 +7,7 @@ namespace CollectApp.ViewModels
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Campo descrição é obrigatório")]
         [StringLength(15, MinimumLength = 3, ErrorMessage = "Descrição deve ter entre 3 e 15 caracteres")]
         [DisplayName("Descrição")]
         public string? Description { get; set; }
