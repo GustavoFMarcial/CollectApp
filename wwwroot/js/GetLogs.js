737eb2bbbe7c1c1e9aad1f7f02528f8653ce4163
GustavoFMarcial <gfm211101@gmail.com>
2025-10-28 20:22:10 -0300

    Adicionar funcionalidade nos botões de paginação da view _LogsModal

--- wwwroot/js/GetLogs.js
+++ wwwroot/js/GetLogs.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     const logList = document.getElementById("log-list");
-    const logPaginationButtos = document.getElementById("logButtonsPagination");
+    const logPaginationButtonsUl = document.getElementById("logButtonsPagination");
+    const logPaginationButtons = document.querySelectorAll(".pagination-log-button");
 
     document.body.addEventListener("click", async (e) => {
-        const button = e.target.closest(".log-list-button");
+        const logButton = e.target.closest(".log-list-button");
 
         async function fetchLogs(url) {
             const response = await fetch(url);
@@ -26,35 +27,54 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         }
 
-        function createPaginationButtons(totalPages){
-            for (let i = 1; i <= totalPages.totalPages; i++){
+        function createPaginationButtons(totalPages) {
+            logPaginationButtonsUl.textContent = "";
+            for (let i = 1; i <= totalPages.totalPages; i++) {
                 const li = document.createElement("li");
                 const button = document.createElement("button");
 
+                if (i == totalPages.pageNum){
+                    button.classList.add("custom-active");
+                }
+
+                button.type = "button";
                 button.value = i;
                 button.innerHTML = i;
-                button.classList.add("page-link", "page-button");
+                button.classList.add("page-link", "page-log-button", "pagination-log-button");
                 li.classList.add("page-item");
 
                 li.appendChild(button);
-                logPaginationButtos.appendChild(li);
+                logPaginationButtonsUl.appendChild(li);
             }
         }
 
-        if (button) {
-            const entity = button.dataset.entity;
-            const id = button.dataset.id;
+        if (logButton) {
+            const entity = logButton.dataset.entity;
+            const id = logButton.dataset.id;
 
             const response = await fetchLogs(`/AuditLog/GetLogs?entityName=${entity}&entityId=${id}`);
-            console.log(response);
             createLogRow(response);
             createPaginationButtons(response);
 
             const modalEl = document.getElementById("logsModal");
-            if (modalEl) {
-                const modal = bootstrap.Modal.getInstance(modalEl) || new bootstrap.Modal(modalEl);
-                modal.show();
-            }
+            modalEl.dataset.entity = entity;
+            modalEl.dataset.id = id;
+
+            const modal = bootstrap.Modal.getInstance(modalEl) || new bootstrap.Modal(modalEl);
+            modal.show();
+        }
+
+        const logPaginationButton = e.target.closest(".pagination-log-button");
+        if (logPaginationButton) {
+            e.preventDefault(); // impede reload
+
+            const modalEl = document.getElementById("logsModal");
+            const entity = modalEl.dataset.entity;
+            const id = modalEl.dataset.id;
+
+            const response = await fetchLogs(`/AuditLog/GetLogs?entityName=${entity}&entityId=${id}&pageNum=${logPaginationButton.value}`);
+            createLogRow(response);
+            createPaginationButtons(response);
         }
     });
 });
