8514affd2f6e61b316a9696741ea708a4774275f
GustavoFMarcial <gfm211101@gmail.com>
2025-11-18 19:18:56 -0300

    Corrigir .sln

--- App.Tests/obj/Debug/net9.0/CollectApp.Tests.AssemblyInfo.cs
+++ App.Tests/obj/Debug/net9.0/CollectApp.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollectApp.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+563993aa516f5d2aed2bb84b4412e41b0bc20c27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+987a7f1ddcb493721fe07e0ce29e94e2452625e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollectApp.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollectApp.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
