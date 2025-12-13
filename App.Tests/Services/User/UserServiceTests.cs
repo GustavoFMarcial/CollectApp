1c6f7c9c23d5e593f8e61d80567d3f26c631fbfa
GustavoFMarcial <gfm211101@gmail.com>
2025-12-13 14:08:32 -0400

    Criar teste unitário SetPagedResultUserListViewModel_WhenHasItems_ShouldReturnListWithItems

--- App.Tests/Services/User/UserServiceTests.cs
+++ App.Tests/Services/User/UserServiceTests.cs
@@ -2,6 +2,7 @@ using CollectApp.Models;
 using CollectApp.Repositories;
 using CollectApp.Services;
 using CollectApp.ViewModels;
+using FluentAssertions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.General;
 using Moq;
@@ -15,5 +16,54 @@ public class UserServiceTests
     public UserServiceTests()
     {
         _userRepoMock = new Mock<IUserRepository>();
+    }
+
+    [Fact]
+    public async Task SetPagedResultUserListViewModel_WhenHasItems_ShouldReturnListWithItems()
+    {
+        var userList = new List<ApplicationUser>
+        {
+            new UserBuilder().Build(),
+            new UserBuilder()
+                .WithId("2")
+                .WithFullName("Joba")
+                .WithRole("Admin")
+                .Build(),
+            new UserBuilder()
+                .WithId("3")
+                .WithFullName("Jatobina")
+                .WithRole("Gestor")
+                .Build(),
+        };
+
+        var filters = new UserFilterViewModel();
+
+        var userListViewModel = userList.Select(u =>
+            new UserListViewModelBuilder()
+            .FromUser(u)
+            .Build())
+            .ToList();
+
+        _userRepoMock
+            .Setup(u => u.ToUserListAsync(It.IsAny<UserFilterViewModel>(), It.IsAny<int>(), It.IsAny<int>()))
+            .ReturnsAsync((userList, 3));
+
+        var service = new UserService(
+            _userRepoMock.Object
+        );
+
+        var result = await service.SetPagedResultUserListViewModel(filters, 1, 10);
+
+        var expected = new PagedResultViewModel<UserListViewModel, UserFilterViewModel>
+        {
+            Items = userListViewModel,
+            TotalPages = 1,
+            PageNum = 1,
+            Filters = filters,
+        };
+
+        result.Should().BeEquivalentTo(expected);
+
+        _userRepoMock.Verify(u => u.ToUserListAsync(It.IsAny<UserFilterViewModel>(), It.IsAny<int>(), It.IsAny<int>()), Times.Once);
     }
 }
