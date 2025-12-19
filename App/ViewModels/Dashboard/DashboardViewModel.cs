843db9af4d26042c2ddc3a93dd34d0431f99d347
GustavoFMarcial <gfm211101@gmail.com>
2025-12-18 20:57:12 -0400

    Criar viewmodel DashboardViewModel

---
+++ App/ViewModels/Dashboard/DashboardViewModel.cs
@@ -0,0 +1,12 @@
+using CollectApp.Dto;
+
+namespace CollectApp.ViewModels;
+
+public class DashboardViewModel
+{
+    public int TotalCollects { get; set; }
+    public int TotalVolume { get; set; }
+    public int TotalWeight { get; set; }
+    public List<CollectPerStatusDto> CollectPerStatusDto { get; set; } = new();
+    public List<CollectPerDayDto> CollectPerDayDto { get; set; } = new();
+}
